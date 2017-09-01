a10f233ebe9e62acdb7b04f383dfea4962437eeb
renpeng <renpeng@jxfls.com>
2017-09-01 09:08:27 +0800

    ver 0.1 ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    
+    <Vueditor v-model="cont" :content='cont' @on-content-change='change' >
+    </Vueditor>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Vueditor from './components/Vueditor';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      cont: '<p>hello vue ueditor </p>',
+    };
+  },
+  methods: {
+    change(val) {
+      this.cont = val;
+      console.log(`编辑器内容Change：${val}`);
+    },
+    editorReady(editorInstance) {
+      editorInstance.addListener('contentChange', () => {
+        console.log(`编辑器内容：${editorInstance.getContent()}`);
+      });
+    },
+  },
   components: {
-    Hello,
+    Vueditor,
   },
 };
 </script>
---
+++ src/components/Vueditor.vue
@@ -0,0 +1,119 @@
+<template>
+  <script :id='id' ref="vueditor" type="text/plain">
+  </script>
+</template>
+
+<script>
+export default {
+  name: 'vue-ueditor',
+  props: {
+    ueditorPath: {
+      type: String,
+      required: false,
+      default: '/static/ueditor/',
+    },
+    content: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+    options: {
+      type: Object,
+      required: false,
+      default: () => { },
+    },
+  },
+  data() {
+    const r = Math.random().toString(10).replace('0.', '');
+    const randomId = `vueditor-${r}`;
+    return {
+      id: randomId,
+      vcontent: '',
+      instance: null,
+      defaultConfig: {
+        toolbars: [
+          ['fullscreen', 'source', 'undo', 'redo', 'bold'],
+        ],
+        autoHeightEnabled: true,
+        autoFloatEnabled: true,
+      },
+    };
+  },
+  mounted() {
+    this.createHeadElementAndLoadJs();
+  },
+  beforeDestroy() {
+    if (this.instance !== null && this.instance.destroy) {
+      this.instance.destroy();
+      this.instance = null;
+    }
+  },
+  methods: {
+    initialize() {
+      if (this.$el) {
+        // let self = this
+        this.createHeadElementAndLoadJs();
+      }
+    },
+    createHeadElementAndLoadJs() {
+      let configScriptElement = document.getElementById('ueditor-config-javascript');
+      let ueditorScriptElement = document.getElementById('ueditor-all-javascript');
+      let langScriptElement = document.getElementById('ueditor-lang-javascript');
+      // let langLinkElement = document.getElementById('ueditor-css-link');
+      // ueditor.config.js
+      if (configScriptElement === null) {
+        configScriptElement = document.createElement('script');
+        configScriptElement.id = 'ueditor-config-javascript';
+        configScriptElement.type = 'text/javascript';
+        configScriptElement.src = `${this.ueditorPath}ueditor.config.js`;
+      }
+
+      // ueditor.all.js
+      if (ueditorScriptElement === null) {
+        ueditorScriptElement = document.createElement('script');
+        ueditorScriptElement.id = 'ueditor-all-javascript';
+        ueditorScriptElement.type = 'text/javascript';
+        ueditorScriptElement.src = `${this.ueditorPath}ueditor.all.js`;
+      }
+      if (langScriptElement === null) {
+        langScriptElement = document.createElement('script');
+        langScriptElement.id = 'ueditor-lang-javascript';
+        langScriptElement.type = 'text/javascript';
+        langScriptElement.src = `${this.ueditorPath}/lang/zh-cn/zh-cn.js`;
+      }
+      console.log(ueditorScriptElement);
+      const s = document.getElementsByTagName('head')[0];
+      s.appendChild(configScriptElement);
+      configScriptElement.onload = () => {
+        s.appendChild(ueditorScriptElement);
+        ueditorScriptElement.onload = () => {
+          this.initUeditorView();
+        };
+      };
+    },
+    initUeditorView() {
+      this.vcontent = this.content;
+      if (this.instance === null) {
+        this.$nextTick(() => {
+          const self = this;
+          if (self.options) {
+            self.instance = window.UE.getEditor(self.id, self.options.config);
+          } else {
+            self.instance = window.UE.getEditor(self.id, self.defaultConfig);
+          }
+          // contentChange
+          self.instance.on('contentChange', () => {
+            self.content = self.instance.getContent();
+            self.$emit('on-content-change', self.content);
+          });
+
+          // emit ready
+          self.$emit('ready', self.instance);
+        });
+      }
+    },
+  },
+};
+</script>
